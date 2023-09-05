d71c1d642da50662ce3973aeac50b1cd94f8c579
Jacob Goldstein <pantingpug@yahoo.com>
2023-09-05 16:59:35 -0400

    Initial Commit

---
+++ HW1/Aboutme.swift
@@ -0,0 +1,14 @@
+//
+//  Aboutme.swift
+//  HW1
+//
+//  Created by Jacob Goldstein on 9/3/23.
+//
+
+import UIKit
+
+// This struct will model the properties for an Aboutme
+struct Aboutme {
+    let info: String
+    let image: UIImage
+}
